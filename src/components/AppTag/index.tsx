a7a5ac7baabb41cf08095d9de9a165883c96bcc2
Alan Graton Lourenço de Brito <graton.alan@gmail.com>
2024-03-19 17:56:53 -0300

    refactor: minor code refactor in `AppTag`

--- src/components/AppTag/index.tsx
+++ src/components/AppTag/index.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 
-import { Pressable, Text } from "react-native";
-
-import { useTheme } from "styled-components/native";
 import * as S from "./styles";
 
 interface IProps {
@@ -12,20 +9,9 @@ interface IProps {
 }
 
 export function AppTag({ title, isChecked = false, disabled = false }: IProps) {
-  const { COLORS } = useTheme();
-
   return (
-    <Pressable
-      style={{
-        backgroundColor: COLORS.GRAY_200,
-        width: 75,
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 100,
-        padding: 6,
-      }}
-    >
-      <Text style={{ color: COLORS.WHITE, fontSize: 10 }}>{title}</Text>
-    </Pressable>
+    <S.Tag>
+      <S.Title>{title}</S.Title>
+    </S.Tag>
   );
 }
--- src/components/AppTag/styles.ts
+++ src/components/AppTag/styles.ts
@@ -1,1 +1,20 @@
 import styled, { css } from "styled-components/native";
+
+export const Tag = styled.Pressable`
+  width: 75px;
+
+  align-items: center;
+  justify-content: center;
+
+  border-radius: 6px;
+
+  padding: 6px;
+
+  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.COLORS.WHITE};
+
+  font-size: 10px;
+`;
