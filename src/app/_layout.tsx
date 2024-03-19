1c293ed99fa90324e38a1b6c83fe7abb6f3d3dc0
Alan Graton Lourenço de Brito <graton.alan@gmail.com>
2024-03-19 16:55:31 -0300

    wip: sets up `Styled-Components` ThemeProvider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,15 @@ import React from "react";
 
 import { Slot } from "expo-router";
 
+import { THEME } from "@/theme";
+import { ThemeProvider } from "styled-components/native";
+import { StatusBar } from "expo-status-bar";
+
 export default function RootLayout() {
-  return <Slot initialRouteName="home" />;
+  return (
+    <ThemeProvider theme={THEME}>
+      <Slot initialRouteName="home" />
+      <StatusBar style="light" animated />
+    </ThemeProvider>
+  );
 }
