d617cadcb9b5ebb494bf19b3ff7b4120c3d18a64
Alan Graton Lourenço de Brito <graton.alan@gmail.com>
2024-03-19 16:54:32 -0300

    wip: creates initial theming

---
+++ src/theme/index.ts
@@ -0,0 +1,37 @@
+export const THEME = {
+  COLORS: {
+    YELLOW_DARK: "#C47F17",
+    YELLOW: "#DBAC2C",
+
+    PURPLE_DARK: "#4B2995",
+    PURPLE: "#8047F8",
+    PURPLE_LIGHT: "#EBE5F9",
+
+    GRAY_100: "#272221",
+    GRAY_200: "#403937",
+    GRAY_300: "#574F4D",
+    GRAY_400: "#8D8686",
+    GRAY_500: "#D7D5D5",
+    GRAY_600: "#E6E5E5",
+    GRAY_700: "#EDEDED",
+    GRAY_800: "#F3F2F2",
+    GRAY_900: "#F3F2F2",
+
+    WHITE: "#FFFFFF",
+
+    RED_DARK: "#C44117",
+    RED: "#E8BAAB",
+    RED_LIGHT: "#F2DFD8",
+  },
+  // FONT_SIZE: {
+  //   xls: 12,
+  //   sm: 14,
+  //   md: 16,
+  //   lg: 20,
+  //   xlg: 24,
+  // },
+  // FONT_FAMILY: {
+  //   HEADING: "Karla_700Bold",
+  //   BODY: "Karla_400Regular",
+  // },
+};
