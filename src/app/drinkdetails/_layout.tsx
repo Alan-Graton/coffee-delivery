2c6b428ede32b97c8c8f05b7ce9f6f7d9a5a8b97
Alan-Graton <graton.alan@gmail.com>
2024-03-20 00:09:33 -0300

    wip: adds navigation in `Drink Details` header

--- src/app/drinkdetails/_layout.tsx
+++ src/app/drinkdetails/_layout.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-import { Stack } from "expo-router";
+import { Stack, router } from "expo-router";
 
 import { AppIconButton } from "@/components/AppIconButton";
+
+import { HandleGoBack } from "../utils/HandleGoBack";
 
 import { ArrowLeft, ShoppingCart } from "phosphor-react-native";
 
@@ -17,12 +19,12 @@ export default function DrinkDetailsLayout() {
         headerShadowVisible: false,
         title: "",
         headerLeft: () => (
-          <AppIconButton>
+          <AppIconButton onPress={HandleGoBack}>
             <ArrowLeft size={24} color={COLORS.WHITE} />
           </AppIconButton>
         ),
         headerRight: () => (
-          <AppIconButton>
+          <AppIconButton onPress={() => router.push("/cart/")}>
             <ShoppingCart size={24} color={COLORS.PURPLE} weight="fill" />
           </AppIconButton>
         ),
