74e4f999e6c630fb5532850add871d82e87d3abf
Alan-Graton <graton.alan@gmail.com>
2024-03-23 20:58:29 -0300

    wip: adds new ui/ux validations in styles files

--- src/components/AppTag/index.tsx
+++ src/components/AppTag/index.tsx
@@ -1,17 +1,32 @@
 import React from "react";
+import { PressableProps } from "react-native";
 
 import * as S from "./styles";
 
-interface IProps {
+interface IProps extends PressableProps {
   title: string;
+  variant?: S.ITagVariant;
   isChecked?: boolean;
   disabled?: boolean;
 }
 
-export function AppTag({ title, isChecked = false, disabled = false }: IProps) {
+export function AppTag({
+  title,
+  variant = "selectable",
+  isChecked = false,
+  disabled = false,
+  ...rest
+}: IProps) {
   return (
-    <S.Tag isChecked={isChecked} disabled={disabled}>
-      <S.Title>{title}</S.Title>
+    <S.Tag
+      isChecked={isChecked}
+      disabled={disabled}
+      variant={variant}
+      {...rest}
+    >
+      <S.Title isChecked={isChecked} disabled={disabled} variant={variant}>
+        {title}
+      </S.Title>
     </S.Tag>
   );
 }
--- src/components/AppTag/styles.ts
+++ src/components/AppTag/styles.ts
@@ -1,34 +1,80 @@
 import styled, { css } from "styled-components/native";
 
-interface IStyleProps {
+export type ITagVariant = "selectable" | "ghost";
+
+interface ITagStyleProps {
+  variant?: ITagVariant;
   isChecked?: boolean;
   disabled?: boolean;
 }
 
-export const Tag = styled.Pressable<IStyleProps>`
-  /* width: 75px; */
-
+export const Tag = styled.Pressable<ITagStyleProps>`
   align-items: center;
   justify-content: center;
 
-  /* padding: 10px; */
-
-  height: 25px;
-
-  /* border-radius: 6px; */
   border-radius: 100px;
 
   padding: 6px;
 
-  background-color: ${({ theme, disabled }) =>
-    !disabled && theme.COLORS.GRAY_200};
+  ${({ theme, variant, isChecked, disabled }) => {
+    if (variant === "ghost") {
+      return `
+        background-color: ${theme.COLORS.PURPLE_LIGHT};
+      `;
+    }
+
+    if (disabled) {
+      return `
+        background-color: ${theme.COLORS.GRAY_200};
+      `;
+    }
+
+    if (variant === "selectable" && isChecked) {
+      return `
+        background-color: ${theme.COLORS.PURPLE_DARK};
+      `;
+    }
+
+    if (variant === "selectable") {
+      return `
+        background-color: transparent;
+        border: 1px solid ${theme.COLORS.PURPLE};
+      `;
+    }
+  }}
 `;
 
-export const Title = styled.Text`
-  ${({ theme }) => css`
-    color: ${theme.COLORS.WHITE};
+export const Title = styled.Text<ITagStyleProps>`
+  text-align: center;
 
+  ${({ theme }) => css`
     font-size: ${theme.FONT_SIZE.TAG}px;
     font-family: ${theme.FONT_FAMILY.TAG};
   `}
+
+  ${({ theme, variant, isChecked, disabled }) => {
+    if (variant === "ghost") {
+      return `
+        color: ${theme.COLORS.PURPLE};
+      `;
+    }
+
+    if (disabled) {
+      return `
+        color: ${theme.COLORS.WHITE};
+      `;
+    }
+
+    if (variant === "selectable" && isChecked) {
+      return `
+        color: ${theme.COLORS.WHITE};
+      `;
+    }
+
+    if (variant === "selectable") {
+      return `
+        color: ${theme.COLORS.PURPLE_DARK};
+      `;
+    }
+  }}
 `;
